51f453d8a3ae4893ce1ee88a726da6220b761c7c
Grace Do <pec7ds@virginia.edu>
2024-01-07 16:17:37 -0500

    updated to run on github pages

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,24 +1,27 @@
-import "../App.css";
+import "/src/App.css";
 
 function Content() {
   return (
     <>
-      <div id="résumé" className="m-0 bg-purple" style={{ minHeight: "100vh" }}>
+      <div id="résumé" 
+        className="m-0 purple" 
+        style={{ minHeight: "100vh" }}
+      >
         <p>hey, my name is grace do.</p>
       </div>
       <div
         id="visual media"
-        className="m-0 bg-secondary bg-opacity-10"
+        className="m-0 off-white"
         style={{ minHeight: "100vh" }}
       >
         hey, my name is grace do.
       </div>
       <div
         id="projects"
-        className="m-0 bg-success bg-opacity-50"
+        className="m-0 green"
         style={{ minHeight: "100vh" }}
       >
-        hey, my name is grace do.
+        <p>hey, my name is grace do.</p>
       </div>
     </>
   );
