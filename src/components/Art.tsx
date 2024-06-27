58c15b183b3d7d7d17df61caf47f0312e6341754
Grace Do <pec7ds@virginia.edu>
2024-06-27 08:08:57 +0000

    Added new empty pages

---
+++ src/components/Art.tsx
@@ -0,0 +1,11 @@
+function Art() {
+    return (
+        <div className="ArtContainer" style={{
+            height: '100vh'
+        }}>
+            
+        </div>
+    )
+
+}
+export default Art;
---
+++ src/components/Software.tsx
@@ -0,0 +1,11 @@
+function Software() {
+    return (
+        <div className="SoftwareContainer" style={{
+            height: '100vh'
+        }}>
+        
+        </div>
+    )
+
+}
+export default Software;
