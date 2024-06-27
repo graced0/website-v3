316f95bdd06e790aa813ec79f3babf799b2b90f3
Grace Do <pec7ds@virginia.edu>
2024-06-27 08:06:36 +0000

    Added in empty future pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import Hero from "./components/Hero";
+import Art from "./components/Art";
+import Software from "./components/Software";
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
         backgroundColor: '#B1DDAD',
       }}>
         <Hero />
+        <Art />
+        <Software />
       </div>
     </div>
   );
